39f9b1741eec3074dff2085f4cb674372d991a93
MihaBEST <mihailkuvanin@gmail.com>
2026-03-15 23:30:07 +0300

    Update __init__.py

--- finbot/__init__.py
+++ finbot/__init__.py
@@ -1,2 +1,2 @@
-from client import FinamClient
-from calculate import get_tickers, get_short_tickers, calculate_indicators
+from finbot.client import FinamClient
+from finbot.calculate import get_tickers, get_short_tickers, calculate_indicators
